58081cad4a3a665e105e391742aeb879585b7077
Etienne <etc@proxem.com>
2016-03-17 01:52:37 +0100

    initial commit, part II

--- src/FacebookWebHooks/Startup.cs
+++ src/FacebookWebHooks/Startup.cs
@@ -26,6 +26,9 @@ namespace FacebookWebHooks
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            services.Configure<FacebookOptions>(Configuration.GetSection(nameof(FacebookOptions)));
+
             // Add framework services.
             services.AddMvc();
         }
